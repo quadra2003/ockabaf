f4437775d5085124cd5c7f96c7f5f64fc59709c8
quadra2003 <56703545+quadra2003@users.noreply.github.com>
2025-06-21 00:20:40 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,14 @@ export default function Layout({ children, title = 'OCKABA Foundation' }) {
         <meta name="description" content="Empowering Korean-American legal professionals through mentorship, education, and opportunity." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+    
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <meta name="msapplication-TileColor" content="#0047A0">
+        <meta name="theme-color" content="#0047A0">
+    
       </Head>
       <div className="min-h-screen bg-white">
         <Navigation />
